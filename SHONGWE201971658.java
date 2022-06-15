af85d19b0106a424585851c396d045c1ff690f1f
Innocent Shongwe <innocent@gmail.com>
2022-06-15 10:46:33 +0200

    First Commit

---
+++ SHONGWE201971658.java
@@ -0,0 +1,20 @@
+public class{
+	public static void main(String [] args) {
+
+		System.out.println("Enter two numbers to multiply them and get an answer");
+		System.out.println("First Number");
+		Input.number= num1;
+		System.out.println("Enter Second number");
+		Input.number=num2;
+
+		total=num1*num2;
+
+		System.out.printLn(" Your First number multiply by the second number is " + total);
+
+
+
+	}
+
+
+
+}
